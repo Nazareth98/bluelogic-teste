8476fd1ac5d9f95cba1a360f16634ff59ec5f337
Vendrick98# <patrick98live@gmail.com>
2024-08-16 15:50:26 -0300

    teste finalizado

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,35 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { ItemsService } from "./items.service";
-import { CreateItemDto } from "./dtos/create-item.dto";
-import { UpdateItemDto } from "./dtos/update-item.dto";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { CreateItemDto } from './dtos/create-item.dto';
+import { UpdateItemDto } from './dtos/update-item.dto';
 
 @Controller('items')
 export class ItemsController {
-  constructor (private itemsService: ItemsService) {}
+  constructor(private itemsService: ItemsService) {}
 
   @Get()
-  findAll(){
-    return this.itemsService.findAll()
+  findAll() {
+    return this.itemsService.findAll();
   }
 
   @Get(':id')
-  findById(@Param('id') id: string){
-    return this.itemsService.findById(Number(id))
+  findById(@Param('id') id: string) {
+    return this.itemsService.findById(Number(id));
   }
 
   @Post()
-  create(@Body() item: CreateItemDto){
-    return this.itemsService.create(item)
+  create(@Body() item: CreateItemDto) {
+    return this.itemsService.create(item);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() item: UpdateItemDto){
-    return this.itemsService.update(Number(id), item)
+  update(@Param('id') id: string, @Body() item: UpdateItemDto) {
+    return this.itemsService.update(Number(id), item);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string){
-    return this.itemsService.deleteById(Number(id))
+  delete(@Param('id') id: string) {
+    return this.itemsService.deleteById(Number(id));
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,42 +1,54 @@
-import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
-import { CreateItemDto } from "./dtos/create-item.dto";
-import { UpdateItemDto } from "./dtos/update-item.dto";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateItemDto } from './dtos/create-item.dto';
+import { UpdateItemDto } from './dtos/update-item.dto';
 
-global.items = []
+global.items = [];
 
 @Injectable()
 export class ItemsService {
-  findAll():any[]{
-    return global.items
+  findAll(): any[] {
+    return global.items;
   }
 
-  findById(id:number){
-    const selectedItem = global.items.find((item) => item.id === id)
-    if (!selectedItem) throw new NotFoundException('Item não encontrado.')
-    return selectedItem
+  findById(id: number) {
+    const selectedItem = global.items.find((item) => item.id === id);
+    if (!selectedItem) throw new NotFoundException('Item não encontrado.');
+    return selectedItem;
   }
 
   create(itemToCreate: CreateItemDto) {
-    if (!itemToCreate.id) throw new BadRequestException('O ID precisa ser um inteiro válido.')
-    if (!itemToCreate.name) throw new BadRequestException('O Nome precisa ser um texto válido.')
-    if (!itemToCreate.description) throw new BadRequestException('A Descrição precisa ser um texto válido.')
-    const itemWithSameId = global.items.find((item) => item.id === itemToCreate.id)
-    if (itemWithSameId) throw new BadRequestException('Já existe um Item com o mesmo ID.')
-    global.items.push(itemToCreate)
+    if (!itemToCreate.id)
+      throw new BadRequestException('O ID precisa ser um inteiro válido.');
+    if (!itemToCreate.name)
+      throw new BadRequestException('O Nome precisa ser um texto válido.');
+    if (!itemToCreate.description)
+      throw new BadRequestException('A Descrição precisa ser um texto válido.');
+    const itemWithSameId = global.items.find(
+      (item) => item.id === itemToCreate.id,
+    );
+    if (itemWithSameId)
+      throw new BadRequestException('Já existe um Item com o mesmo ID.');
+    global.items.push(itemToCreate);
   }
 
   update(id: number, itemToUpadate: UpdateItemDto) {
-    if (!itemToUpadate.name) throw new BadRequestException('O Nome precisa ser um texto válido.')
-    if (!itemToUpadate.description) throw new BadRequestException('A Descrição precisa ser um texto válido.')
+    if (!itemToUpadate.name)
+      throw new BadRequestException('O Nome precisa ser um texto válido.');
+    if (!itemToUpadate.description)
+      throw new BadRequestException('A Descrição precisa ser um texto válido.');
     const itemIndex = global.items.findIndex((item) => item.id === id);
     if (itemIndex === -1) throw new NotFoundException('Item não encontrado.');
     const updatedItem = { ...global.items[itemIndex], ...itemToUpadate };
-    global.items[itemIndex] = updatedItem
-    return updatedItem
+    global.items[itemIndex] = updatedItem;
+    return updatedItem;
   }
 
-  deleteById(id: number){
-    const itemIndex = global.items.findIndex(item => item.id === id);
+  deleteById(id: number) {
+    const itemIndex = global.items.findIndex((item) => item.id === id);
     if (itemIndex === -1) throw new NotFoundException('Item não encontrado.');
     global.items.splice(itemIndex, 1);
   }
